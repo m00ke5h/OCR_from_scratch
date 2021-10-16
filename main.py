009163beb1cb076c94d9d1eef2e62857efc10643
m-ar-sh-h <pkdash1947.er@gmail.com>
2021-10-16 22:08:01 +0530

    Predicted accuracy

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
-from math import dist
+TEST_DIR = 'test/'
+DEBUG = True
+
+if DEBUG:
+    from PIL import Image
+    import numpy as np
+
+    def read_image(path):
+        return np.asarray(Image.open(path).convert('L'))
+
+    def write_image(image, path):
+        img = Image.fromarray(np.array(image), 'L')
+        img.save(path)
+
 
 TEST_DATA = 'data/t10k-images-idx3-ubyte'
 TEST_LABELS = 'data/t10k-labels-idx1-ubyte'
@@ -65,7 +78,11 @@ def get_distances_for_test_sample(X_train, test_sample):
     return [euclidean_distance(train_sample, test_sample) for train_sample in X_train]
 
 
-def knn(X_train, Y_train, X_test, Y_test, k=3):
+def get_most_frequent_element(l):
+    return max(l, key=l.count)
+
+
+def knn(X_train, Y_train, X_test, k=3):
     y_pred = []
     for test_sample_idx, test_sample in enumerate(X_test):
         training_distances = get_distances_for_test_sample(X_train, test_sample)
@@ -79,19 +96,24 @@ def knn(X_train, Y_train, X_test, Y_test, k=3):
         candidates = [
             bytes_to_int(Y_train[idx])
             for idx in sorted_distance_indices[:k]
-        ]    
-        print(f'Point is {bytes_to_int(Y_test[test_sample_idx])} and we guessed {candidates}')
-        y_sample = 5
-        y_pred.append(y_sample)
+        ]
+        # print(f'Point is {bytes_to_int(Y_test[test_sample_idx])} and we guessed {candidates}')
+        # y_sample = 5
+        top_canditate = get_most_frequent_element(candidates)   
+        y_pred.append(top_canditate)
     return y_pred    
 
 
 def main():
     X_train = read_images(TRAINING_DATA, 1000)
-    Y_train = read_labels(TRAINING_LABELS)
+    Y_train = read_labels(TRAINING_LABELS, 1000)
     X_test = read_images(TEST_DATA, 5)
-    Y_test = read_labels(TEST_LABELS)
+    Y_test = read_labels(TEST_LABELS, 5)
     # print(len(X_train), len(Y_train), len(X_test), len(Y_test), sep='\n')
+
+    if DEBUG:
+        for idx, test_sample in enumerate(X_test):
+            write_image(test_sample, f'{TEST_DIR}{idx}.png')
 
     X_train = extract_features(X_train)
     X_test = extract_features(X_test)
@@ -99,7 +121,12 @@ def main():
     # print(len(X_train[0]))
     # print(len(X_test[0]))
 
-    knn(X_train, Y_train, X_test, Y_test)
+    y_pred = knn(X_train, Y_train, X_test)
+    print(y_pred)
+    accuracy = sum([int(y_pred_i == bytes_to_int(y_test_i)) for y_pred_i, y_test_i in zip(y_pred, Y_test)]) / len(Y_test)
+    print(accuracy*100, "%")
+
+
 
 if __name__ == '__main__':
     main()
